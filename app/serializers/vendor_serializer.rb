76bc70fd24d77b471033ed83e4c02fcd99ed22a3
Matt Willhite <matthew.willhite@gmail.com>
2014-10-03 17:34:27 -0500

    Outputting floats for the lat/lon

--- app/serializers/vendor_serializer.rb
+++ app/serializers/vendor_serializer.rb
@@ -5,4 +5,12 @@ class VendorSerializer < ActiveModel::Serializer
   has_many :breweries
 
   embed :ids, include: true
+
+  def lat
+    object.lat.to_f
+  end
+
+  def lon
+    object.lon.to_f
+  end
 end
